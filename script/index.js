8f4465f856f28c98d786d14ba56d2ab1e0f88b81
Hiro <hiro527.dev@gmail.com>
2022-01-23 10:43:15 +0900

    [Change] スタイルの修正、不要なバッジの削除

--- script/index.js
+++ script/index.js
@@ -74,7 +74,6 @@ window.onload = () => {
             platforms.shift();
         })
         document.getElementById('releaseLink').setAttribute('href', releaseInfo.url);
-        document.getElementById('releaseBadge').setAttribute('src', `https://img.shields.io/badge/${releaseInfo.version}-Release%20Note-brightgreen`)
         document.getElementById('versionText').innerText = releaseInfo.version
         document.getElementById('iconHolder').style.animationPlayState = 'running'
         setTimeout(() => {
